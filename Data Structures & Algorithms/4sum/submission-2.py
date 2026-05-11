e23cd036d1cc1d7aae7e613b09e573c1739a92f8
Evan Law <17935481+evanwlaw@users.noreply.github.com>
2026-05-11 00:09:30 -0400

    Add: 4sum - submission-2

---
+++ Data Structures & Algorithms/4sum/submission-2.py
@@ -0,0 +1,45 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        """
+        solve for ksum recusively
+
+        if k == 2 -> solve for 2sum
+
+
+
+        """
+        nums.sort()
+        res = []
+        quad = []
+
+        def ksum(k, start, target):
+            if k == 2:
+                # solve for 2 sum
+                l, r = start, len(nums) - 1
+
+                while l < r:
+                    if nums[l] + nums[r] < target:
+                        l += 1
+                    elif nums[l] + nums[r] > target:
+                        r -= 1
+                    else:
+                        res.append(quad + [nums[l], nums[r]])
+                        l += 1
+
+                        while l < r and nums[l] == nums[l - 1]:
+                            l += 1
+                return
+            
+            for i in range(start, len(nums) - k + 1):
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+                
+                quad.append(nums[i])
+                ksum(k - 1, i + 1, target - nums[i])
+                quad.pop()
+        
+        ksum(4, 0, target)
+        return res
+
+            
+        
