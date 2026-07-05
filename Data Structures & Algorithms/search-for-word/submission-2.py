f7924bacc0b622c3652e12b230059d3f3f1974e8
Evan Law <17935481+evanwlaw@users.noreply.github.com>
2026-07-05 18:32:26 -0400

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.py
@@ -0,0 +1,39 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        '''
+        dfs backtrack.
+
+        iterate through grid. 
+            if board[r][c] == start of word,
+                dfs
+        '''
+        ROWS, COLS = len(board), len(board[0])
+        def dfs(r,c,i):
+            if i == len(word):
+                return True
+
+            if (r < 0 or r >= ROWS or
+                c < 0 or c >= COLS or
+                board[r][c] != word[i]
+            ):
+                return False
+            
+            temp = board[r][c]
+            board[r][c] = '#'
+            search = (
+                dfs(r - 1, c, i + 1) or
+                dfs(r + 1, c, i + 1) or
+                dfs(r, c - 1, i + 1) or
+                dfs(r, c + 1, i + 1)
+            )
+            board[r][c] = temp
+
+            return search
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == word[0]:
+                    if dfs(r,c,0):
+                        return True
+
+        return False
