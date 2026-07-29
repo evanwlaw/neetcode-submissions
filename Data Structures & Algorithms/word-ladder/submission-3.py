5d2a9823388aa746c287c311b9f93612b1f0eb62
Evan Law <17935481+evanwlaw@users.noreply.github.com>
2026-07-29 17:40:11 -0400

    Add: word-ladder - submission-3

---
+++ Data Structures & Algorithms/word-ladder/submission-3.py
@@ -0,0 +1,57 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+
+        output = 1
+        # create adjacency map - wildcards : word
+        adj = defaultdict(list)
+        wordList.append(beginWord) # append starting word to create adj list from all words
+
+        for word in wordList:
+            for c in range(len(word)):
+                adj[word[:c] + "*" + word[c + 1:]].append(word)
+
+        set1 = {beginWord}
+        set2 = {endWord}
+        visited = set()
+        visited.add(beginWord)
+        visited.add(endWord)
+        while set1 and set2:
+
+            if len(set1) > len(set2):
+                set1, set2 = set2, set1
+            output += 1
+            nextFronter = set()
+            for word in set1:
+                for c in range(len(word)):
+                    root = word[:c] + "*" + word[c + 1:]
+                    for neiWord in adj[root]:
+                        if neiWord in set2:
+                            return output
+                        if neiWord in visited:
+                            continue
+                        visited.add(neiWord)
+                        nextFronter.add(neiWord)
+            set1 = nextFronter
+        # # bfs
+        # queue = deque([beginWord])
+        # visited = {beginWord}
+        #
+        # while queue:
+        #     for i in range(len(queue)):
+        #         word = queue.popleft()
+        #         # base case
+        #         if word == endWord:
+        #             return output
+        #
+        #         # go through all patterns of word
+        #         for c in range(len(word)):
+        #             root = word[:c] + "*" + word[c + 1:]
+        #
+        #             for neiWord in adj[root]:
+        #                 if neiWord not in visited:
+        #                     queue.append(neiWord)
+        #                     visited.add(neiWord)
+        #     output += 1
+        return 0
