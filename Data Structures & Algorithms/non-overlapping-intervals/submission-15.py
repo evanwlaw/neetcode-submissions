b7b034b803cf0dca4662342f96f9523b17312830
Evan Law <17935481+evanwlaw@users.noreply.github.com>
2026-08-12 22:46:16 -0400

    Add: non-overlapping-intervals - submission-15

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-15.py
@@ -0,0 +1,42 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        """
+        1   2   4
+        ---- 
+            -----
+        ---------
+        Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+        Output: 1
+        Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
+
+        1   2   3   4
+        ---
+            ---
+        -------
+                ----
+
+
+        intervals=[[1,100],[11,22],[1,11],[2,12]]
+
+         1,11
+         2,12
+        11,22
+        1,100
+
+        sort by last interval
+
+        if intervals[i][0] < intervals[i - 1][0] 
+            res += 1
+        """
+        intervals.sort(key=lambda x: x[1])
+        prev = [intervals[0][0], intervals[0][1]]
+        output = 0
+
+        for i in range(1, len(intervals)):
+            
+            if intervals[i][0] < prev[1]:
+                output += 1
+            else:
+                prev[0] = intervals[i][0]
+                prev[1] = intervals[i][1]
+        return output
