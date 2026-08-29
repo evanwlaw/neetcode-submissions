57125c1a26a1023f7d7654042397ccc1bc6c7c7a
Evan Law <17935481+evanwlaw@users.noreply.github.com>
2026-08-29 16:14:29 -0400

    Add: subsets - submission-13

---
+++ Data Structures & Algorithms/subsets/submission-13.py
@@ -0,0 +1,28 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        """
+        get subsets of nums
+
+                                            []
+                            [1]                                 []
+                    [1,2]           [1]                   [2]           []
+                [1,2,3] [1,2]   [1,3]      [1]      [2,3]   [2]    [3]     []
+        """
+        output = []
+        subset = []
+        # decision to use or not
+        def dfs(i):
+            if i >= len(nums):
+                output.append(subset.copy())
+                return
+
+            subset.append(nums[i])
+
+            # with
+            dfs(i+1)
+            subset.pop()
+
+            # without
+            dfs(i + 1)
+        dfs(0)
+        return output
