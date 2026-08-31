98f04b88a57eb429cee2f531e7d9c483267cd0a9
Evan Law <17935481+evanwlaw@users.noreply.github.com>
2026-08-30 22:01:16 -0400

    Add: merge-intervals - submission-8

---
+++ Data Structures & Algorithms/merge-intervals/submission-8.py
@@ -0,0 +1,42 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        """
+        Input: intervals = [[1,3],[1,5],[6,7]]
+        Output: [[1,5],[6,7]]
+
+        1   2   3   4   5   6   7
+        ---------
+        -----------------
+                            -----
+
+
+        Input: intervals = [[1,2],[2,3]]
+        Output: [[1,3]]
+
+        1   2   3
+        -----
+            -----
+
+
+        No guarantee that intervals is sorted. If we iterate through it, we won't be able to see if curr interval has overlap with previous unless we sort intervals by start.
+
+        Once sorted by start,
+            if current interval start <= previous interval end -> found overlap
+                merge by finding which one has a larger end intervals (sorted by start already)
+                append to output
+            else -> it's not overlapping
+                append to output
+        """
+        if len(intervals) <= 1:
+            return intervals
+
+        intervals.sort()
+
+        output = [intervals[0]]
+        
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= output[-1][1]:
+                output[-1][1] = max(output[-1][1], intervals[i][1])
+            else:
+                output.append(intervals[i])
+        return output
